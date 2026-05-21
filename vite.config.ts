e0b81023b412c0f822804add5852ff37e6ae4e52
TBody8 <jugaralbaloncestomola@gmail.com>
2026-05-21 16:33:22 +0200

    fix(pwa): generate correctly sized icons and add maskable variant to fix oversized mobile icon

--- vite.config.ts
+++ vite.config.ts
@@ -41,17 +41,17 @@ export default defineConfig({
         start_url: '/',
         icons: [
           {
-            src: 'fav.png',
+            src: 'pwa-192x192.png',
             sizes: '192x192',
             type: 'image/png',
           },
           {
-            src: 'fav.png',
+            src: 'pwa-512x512.png',
             sizes: '512x512',
             type: 'image/png',
           },
           {
-            src: 'fav.png',
+            src: 'pwa-512x512-maskable.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'maskable',
